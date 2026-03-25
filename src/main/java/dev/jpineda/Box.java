abfaf691b935bb8468124398002a5fa314c6c813
Jaime Pineda <jpin730@pm.me>
2026-03-25 17:47:50 -0600

    feat: :sparkles: add Box class and update Main to demonstrate usage

---
+++ src/main/java/dev/jpineda/Box.java
@@ -0,0 +1,24 @@
+package dev.jpineda;
+
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "value=" + value +
+                '}';
+    }
+}
--- src/main/java/dev/jpineda/Main.java
+++ src/main/java/dev/jpineda/Main.java
@@ -1,7 +1,24 @@
 package dev.jpineda;
 
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        System.out.println("Hello world!");
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("John");
+        names.add("Jane");
+
+        for (String name : names) {
+            System.out.println(name.toUpperCase());
+        }
+
+        Box<String> stringBox = new Box<>("John");
+
+        System.out.println("stringBox = " + stringBox);
+
+        Box<Integer> integerBox = new Box<>(0);
+
+        System.out.println("integerBox = " + integerBox);
     }
 }
